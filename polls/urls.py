a24436a9443b20fe49895d1688484f7c90f522f2
Ujjwal Tak <ujjwal.tak@gmail.com>
2017-10-12 08:27:27 +0530

    test started

--- polls/urls.py
+++ polls/urls.py
@@ -15,7 +15,13 @@ raw_urlpatterns = [
 v1_urlpatterns = [
     url(r'^$', v1_views.index, name='index'),
     url(r'^(?P<question_id>[0-9]+)/$', v1_views.detail, name='detail'),
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^(?P<question_id>[0-9]+)/results/$', v1_views.results, name='results'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', v1_views.vote, name='vote'),
+]
+class_urlpatterns = [
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
 
@@ -24,12 +30,16 @@ v1_urlpatterns = [
 urlpatterns = [
     url(r'^raw/', include(raw_urlpatterns, namespace='raw')),
     url(r'^v1/', include(v1_urlpatterns, namespace='v1')),
+    url(r'^class/', include(v1_urlpatterns, namespace='class')),
     # ex: /polls/
-    url(r'^$', views.index, name='index'),
+    # url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
     # ex: /polls/5/
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    # url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
     # ex: /polls/5/results/
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
+    # url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
     # ex: /polls/5/vote/
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
--- polls/views/__init__.py
+++ polls/views/__init__.py
@@ -1,32 +1,96 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
-from django.http import HttpResponse
+import importlib
+from django.utils import timezone
+from django.views import generic
 
+from polls.models import Question
 from polls.views import raw_views, v1_views
+
 
 # Create your views here.
 def index(request):
-    if request.GET.get('version', '') == 'raw':
-        resp = raw_views.index(request)
-    else:
-        resp = v1_views.index(request)
-
-    return resp
+    mode = request.GET.get('version', 'v1')
+    mode_views = ".%s_views" % mode
+    views_module = importlib.import_module(mode_views, 'polls.views')
+    return views_module.index(request)
 
 
 def detail(request, question_id):
-    if request.GET.get('version', '') == 'raw':
-        resp = raw_views.detail(request, question_id)
-    else:
-        resp = v1_views.detail(request, question_id)
-    return resp
+    mode = request.GET.get('version', 'v1')
+    mode_views = ".%s_views" % mode
+    views_module = importlib.import_module(mode_views, 'polls.views')
+    return views_module.detail(request, question_id)
 
 
 def results(request, question_id):
-    return raw_views.results(request, question_id)
+    mode = request.GET.get('version', 'v1')
+    mode_views = ".%s_views" % mode
+    views_module = importlib.import_module(mode_views, 'polls.views')
+    return views_module.results(request, question_id)
 
 
 def vote(request, question_id):
-    return raw_views.vote(request, question_id)
+    mode = request.GET.get('version', 'v1')
+    mode_views = ".%s_views" % mode
+    views_module = importlib.import_module(mode_views, 'polls.views')
+    return views_module.vote(request, question_id)
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset_old(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+    def get_queryset(self):
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
+
+    def get(self, request, *args, **kwargs):
+        mode = request.GET.get('version')
+        if request.GET and mode and mode in ['raw', 'v1']:
+            resp = index(request)
+        else:
+            resp = super(IndexView, self).get(request)
+        return resp
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+    def get(self, request, *args, **kwargs):
+        mode = request.GET.get('version')
+        if request.GET and mode and (mode in ['raw', 'v1']):
+            resp = detail(request, args[0])
+        else:
+            resp = super(DetailView, self).get(request, *args, **kwargs)
+        return resp
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+    def get(self, request, *args, **kwargs):
+        mode = request.GET.get('version')
+        if request.GET and mode and (mode in ['raw', 'v1']):
+            resp = results(request, args[0])
+        else:
+            resp = super(ResultsView, self).get(request, *args, **kwargs)
+        return resp
