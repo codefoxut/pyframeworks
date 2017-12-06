98682a04ba6d8e99a5367ddb41221d777bba1759
Ujjwal Tak <ujjwal.tak@gmail.com>
2017-12-06 10:33:04 +0530

    favicon.ico addded

--- django_proj/settings/__init__.py
+++ django_proj/settings/__init__.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'polls.apps.PollsConfig',
+    'polls',
     'blog'
 ]
 
@@ -120,8 +120,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/'
+STATIC_ROOT = PROJECT_DIR + '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_DIR, "static"),
+]
 
 try:
-    from dev import *
+    from .dev import *
 except ImportError:
     pass
