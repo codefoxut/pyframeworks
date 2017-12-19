5f70406befe8d3cd1082d28e91178c16c4eeb631
Ujjwal Tak <ujjwal.tak@gmail.com>
2017-12-20 01:26:25 +0530

    DG: homework- creating comment model

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,8 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from blog.models import Post
+from blog.models import Post, Comment
 # Register your models here.
 admin.site.register(Post)
+
+admin.site.register(Comment)
--- blog/views.py
+++ blog/views.py
@@ -4,8 +4,11 @@ from __future__ import unicode_literals
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-from blog.models import Post
-from blog.forms import PostForm
+
+from blog.forms import PostForm, CommentForm
+from blog.models import Post, Comment
+
+
 # Create your views here.
 
 
@@ -80,3 +83,32 @@ def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
     return redirect('post_list')
+
+
+@login_required
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
+
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.delete()
+    return redirect('post_detail', pk=comment.post.pk)
