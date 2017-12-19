aa49045e214545153b9cae20ce968fcc44ac4dc3
Ujjwal Tak <ujjwal.tak@gmail.com>
2017-12-20 00:42:17 +0530

    DG: homework add more

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     url(r'^(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
     url(r'^new/$', views.post_new, name='post_new'),
+    url(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
+    url(r'^(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
+    url(r'^(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
 ]
--- django_proj/urls.py
+++ django_proj/urls.py
@@ -19,7 +19,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^blog/', include('blog.urls')),
+    url(r'^post/', include('blog.urls')),
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
 
