8b1d1e7a2c974ae71f18300cec74578d081f10a2
Ujjwal Tak <ujjwal.tak@gmail.com>
2017-12-25 16:10:26 +0530

    restFrameWork: 1. Serialization updated

--- django_proj/settings/__init__.py
+++ django_proj/settings/__init__.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'blog',
     'rest_framework',
     'rest_api',
+    'snippets',
 ]
 
 MIDDLEWARE = [
--- rest_api/urls.py
+++ rest_api/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url, include
 from rest_framework import routers
+
 from rest_api import views
+from rest_api.snippets import views as snippets_views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -10,7 +12,7 @@ router.register(r'groups', views.GroupViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^snippets/$', views.snippet_list),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
+    url(r'^snippets/$', snippets_views.snippet_list),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', snippets_views.snippet_detail),
 
 ]
