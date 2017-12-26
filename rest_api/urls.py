aee603637d04667995a6f53ee718f024b228cef7
Ujjwal Tak <ujjwal.tak@gmail.com>
2017-12-26 10:17:55 +0530

    requirement.txt and urls.py updated

--- rest_api/urls.py
+++ rest_api/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url, include
 from rest_framework import routers
-from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.schemas import get_schema_view
 
 from rest_api.users_api.views import UserViewSet, GroupViewSet
 from rest_api.snippets_api.views import SnippetViewSet, SnippetUserViewSet
@@ -13,9 +13,14 @@ router.register(r'auth-groups', GroupViewSet)
 router.register(r'users', SnippetUserViewSet)
 router.register(r'snippets', SnippetViewSet)
 
+
+
+schema_view = get_schema_view(title='Pastebin API')
+
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
+    url(r'^schema/$', schema_view),
     url(r'^', include(router.urls)),
     url(r'^drf/', include('rest_api.snippets_api.urls')),
 ]
