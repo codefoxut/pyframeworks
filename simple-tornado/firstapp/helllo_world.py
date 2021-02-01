fa21fc85c806095ae18a2228822138b63d58f124
Ujjwal Tak <ujjwal.tak@go-mmt.com>
2021-02-01 10:51:23 +0530

    tornado first page

---
+++ simple-tornado/firstapp/helllo_world.py
@@ -0,0 +1,29 @@
+import tornado.ioloop
+import tornado.web
+
+from tornado.httpclient import AsyncHTTPClient
+from tornado import gen
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, world")
+
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+    ])
+
+
+@gen.coroutine
+def async_fetch_gen(url):
+    http_client = AsyncHTTPClient()
+    response = yield http_client.fetch(url)
+    raise gen.Return(response.body)
+
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
