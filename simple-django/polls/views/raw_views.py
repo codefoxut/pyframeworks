1e1e0430a863ee091880b0ee303fb43bc13a8e98
Ujjwal Tak <24545193+codefoxut@users.noreply.github.com>
2022-05-25 18:21:08 +0530

    Update raw_views.py

--- simple-django/polls/views/raw_views.py
+++ simple-django/polls/views/raw_views.py
@@ -6,7 +6,7 @@ __all__ = ['index', 'detail', 'results', 'vote']
 
 from django.shortcuts import render
 from django.http import HttpResponse
-from html import escape
+import html
 
 # Create your views here.
 def index(request):
@@ -14,13 +14,13 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % escape(question_id))
+    return HttpResponse("You're looking at question %s." % html.escape(question_id))
 
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
-    return HttpResponse(response % escape(question_id))
+    return HttpResponse(response % html.escape(question_id))
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % escape(question_id))
+    return HttpResponse("You're voting on question %s." % html.escape(question_id))
